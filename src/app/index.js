2bd0b3d40a48507cb8f0d038844f872a56c87a2f
rahulb11 <rahulbhandari11@gmail.com>
2017-12-30 16:55:49 -0600

    Created basic redux code

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,34 @@
-console.log("It works!");
+import { createStore } from "redux";
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "ADD":
+            state = state + action.payload;
+            break;
+        case "SUBTRACT":
+            state = state - action.payload;
+            break;
+    }
+    return state;
+};
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+    console.log("Store updated!", store.getState());
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 22
+});
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 80
+});
